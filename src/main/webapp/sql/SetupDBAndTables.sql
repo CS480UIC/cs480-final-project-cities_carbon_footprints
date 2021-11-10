2bca1da63252239e6c1d489ae74dcd4a09598b03
eleon23 <eleon23@uic.edu>
2021-11-09 21:21:05 -0600

    Added csv and updated create tables queries for carbon_emission and factory

--- src/main/webapp/sql/SetupDBAndTables.sql
+++ src/main/webapp/sql/SetupDBAndTables.sql
@@ -23,7 +23,7 @@ CREATE TABLE entity1
 
 CREATE TABLE city
 (
-	city_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	city_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
 	city_name VARCHAR(40) UNIQUE NOT NULL,
 	population INT,
 	most_used_transportation VARCHAR(40),
@@ -34,7 +34,7 @@ CREATE TABLE city
 
 CREATE TABLE vehicle 
 (
-	vehicle_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	vehicle_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
 	vehicle_type VARCHAR(20) NOT NULL,
 	vehicle_emission_type VARCHAR(10) NOT NULL,
 	gas_mileage DOUBLE(65,2),
@@ -43,15 +43,16 @@ CREATE TABLE vehicle
 	PRIMARY KEY(vehicle_id),
 	FOREIGN KEY(vehicle_city) REFERENCES city(city_name)
 		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY(vehicle_emission_type) REFERENCES carbon_emission(emission_type)
+		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
---	FOREIGN KEY(vehicle_emission_type) REFERENCES carbon_emission(emission_type)
---		ON DELETE CASCADE
---		ON UPDATE CASCADE
+
 
 CREATE TABLE public_transit
 (
-	transit_id INT UNSIGNED AUTO_INCREMENT NOT NULL ,
+	transit_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL ,
 	total_usage DECIMAL(5,2),
 	transit_emission_type VARCHAR(10) NOT NULL,
 	transit_city VARCHAR(40) NOT NULL,
@@ -60,14 +61,35 @@ CREATE TABLE public_transit
 	PRIMARY KEY(transit_id),
 	FOREIGN KEY(transit_city) REFERENCES city(city_name)
 		ON DELETE CASCADE
-		ON UPDATE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY(transit_emission_type) REFERENCES carbon_emission(emission_type)
+		ON DELETE RESTRICT
+		ON UPDATE RESTRICT
 );
 
---	FOREIGN KEY(transit_emission_type) REFERENCES carbon_emission(emission_type)
---		ON DELETE RESTRICT
---		ON UPDATE RESTRICT
+CREATE TABLE factory
+(
+	factory_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
+	factory_name VARCHAR(50) NOT NULL,
+	factory_city VARCHAR(40) NOT NULL,
+	factory_emission_type VARCHAR(10) NOT NULL,
+	factory_date DATE,
+	PRIMARY KEY(factory_id),
+	FOREIGN KEY(factory_city) REFERENCES city(city_name)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY(factory_emission_type) REFERENCES carbon_emission(emission_type)
+		ON DELETE RESTRICT
+		ON UPDATE RESTRICT
+);
 
-
-
+CREATE TABLE carbon_emission
+(
+	emission_source VARCHAR(50) NOT NULL,
+	emission_percent DECIMAL(3,2) NOT NULL,
+	emission_type VARCHAR(10) NOT NULL,
+	emission_date DATE,
+	PRIMARY KEY(emission_type, emission_date)
+);
 
 
