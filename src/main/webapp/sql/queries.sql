a4e87f49189ca1bbf91d776822f170fa8b0b54fc
eleon23 <eleon23@uic.edu>
2021-11-11 16:56:45 -0600

    added queries from 6.2 back

--- src/main/webapp/sql/queries.sql
+++ src/main/webapp/sql/queries.sql
@@ -30,3 +30,7 @@ ON factory(factory_name);
 
 CREATE INDEX FifthIndex
 ON carbon_emission(emission_percent);
+
+SELECT * FROM cities_carbon_footprints.city WHERE YEAR(city_date) = '2021';
+
+SELECT LOWER(vehicle_type), vehicle_city FROM cities_carbon_footprints.vehicle WHERE vehicle_city = 'Chicago';
