27d8acf6b4a8a9a1d788f1cfe33d27bf95b43d72
eleon23 <eleon23@uic.edu>
2021-11-28 11:11:22 -0600

    moved sql file to the directory with the init service, also make the initalize button work

--- src/main/java/init/dao/InitDao.java
+++ src/main/java/init/dao/InitDao.java
@@ -1,5 +1,8 @@
 package init.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,6 +11,9 @@ import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import org.apache.jasper.tagplugins.jstl.core.Url;
 
 import user.domain.User;
 
@@ -29,36 +35,6 @@ public class InitDao {
 	private String MySQL_password = "CS480ROCKS";  //TODO change password
 	
 	/**
-	 * get the Search result with Username 
-	 */
-	public User findByUsername(String username) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		User user = new User();
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cities_carbon_footprints", MySQL_user, MySQL_password);
-		    String sql = "select * from user where username=?";
-		    PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1,username);
-		    ResultSet resultSet = preparestatement.executeQuery();
-		    while(resultSet.next()){
-		    	String user_name = resultSet.getString("username");
-		    	if(user_name.equals(username)){
-		    		user.setUsername(resultSet.getString("username"));
-		    		user.setPassword(resultSet.getString("password"));
-		    		user.setEmail(resultSet.getString("email"));
-		    		
-		    	}
-		    }
-		
-		    connect.close();
-		} catch(SQLException e) {
-			throw new RuntimeException(e);
-		}
-		return user;
-	}
-	
-	/**
 	 * InitliazeDB
 	 */
 	public void intialize() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
@@ -66,59 +42,25 @@ public class InitDao {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cities_carbon_footprints", MySQL_user, MySQL_password);
 			
-			String sql = "CREATE TABLE TEST(id int, name varchar(10))";
-			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.executeUpdate();
-		    connect.close();
-		} catch(SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	
-	
-	/**
-	 * insert User
-	 */
-	public void add(User user) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cities_carbon_footprints", MySQL_user, MySQL_password);
+			URL path  = InitDao.class.getResource("initializeDB.sql");
+			File file = new File(path.getFile());
+			System.out.println(file.getPath());
 			
-			String sql = "insert into user values(?,?,?)";
-			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1,user.getUsername());
-		    preparestatement.setString(2,user.getPassword());
-		    preparestatement.setString(3,user.getEmail());
-		    preparestatement.executeUpdate();
+			Scanner read = new Scanner(file);
+			read.useDelimiter(";");
+			String sqlCmd;
+			
+			while(read.hasNext()) {
+				sqlCmd = read.next();
+				System.out.println(sqlCmd);
+				PreparedStatement preparestatement = connect.prepareStatement(sqlCmd); 
+			    preparestatement.execute();
+			}
+			
+			read.close();
 		    connect.close();
-		} catch(SQLException e) {
+		} catch(SQLException | FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	
-	public List<Object> findall() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
-		List<Object> list = new ArrayList<>();
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cities_carbon_footprints", MySQL_user, MySQL_password);
-			String sql = "select * from user";
-			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-			ResultSet resultSet = preparestatement.executeQuery();			
-			while(resultSet.next()){
-				User user = new User();
-				user.setUsername(resultSet.getString("username"));
-	    		user.setPassword(resultSet.getString("password"));
-	    		user.setEmail(resultSet.getString("email"));
-	    		list.add(user);
-			 }
-			connect.close();
-		} catch(SQLException e) {
-			throw new RuntimeException(e);
-		}
-		return list;
-		
-	}
-		
+	}		
 }
